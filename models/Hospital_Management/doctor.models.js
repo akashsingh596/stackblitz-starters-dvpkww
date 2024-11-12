80e72e0326f1330ca157fdfc8fb4aa76bd4a792c
akashsingh596 <singh.akash211199@gmail.com>
2024-11-12 10:35:16 +0530

    Compete Schema

--- models/Hospital_Management/doctor.models.js
+++ models/Hospital_Management/doctor.models.js
@@ -1,7 +1,32 @@
 import mongoose from "mongoose";
 
-const doctorSchema = new mongoose.Schema({},{timestamps:true});
 
-export const Doctor = mongoose.model("Doctor",doctorSchema);
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    salary: {
+        type: String,
+        required: true,
+
+    },
+    qualification: {
+        type: String,
+        required: true
+    },
+    exprienceInYears: {
+        type: Number,
+        default: 0
+    },
+    workInHospital: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Hospital'
+        }
+    ]
+}, { timestamps: true });
+
+export const Doctor = mongoose.model("Doctor", doctorSchema);
 
 
--- models/Hospital_Management/patient.models.js
+++ models/Hospital_Management/patient.models.js
@@ -1,6 +1,38 @@
 import mongoose from "mongoose";
 
-const patientSchema = new mongoose.Schema({}, { timestamps: true });
+const patientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+
+    },
+    diagonosedWith: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    boodGroup: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+        enum: ["M", "F", "O"]
+    },
+    admitedIn: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Hospital",
+        required: true,
+    },
+}, { timestamps: true });
 
 export const Patient = mongoose.model("Patient", patientSchema);
 
